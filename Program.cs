02b5f43716dc1a2a482c316498c8595255cf790b
Vladyslav Taranov <wvlad.dev@gmail.com>
2020-12-24 22:05:08 +0200

    try for weeks

--- Program.cs
+++ Program.cs
@@ -23,35 +23,44 @@ namespace DepositTermCalc
 
         class Period
         {
-            public DateTime Month { get; }
+            public DateTime Start { get; }
             public decimal Balance { get; set; }
 
-            public Period(DateTime month)
+            public Period(DateTime start)
             {
-                Month = month;
+                Start = start;
             }
         }
 
         static void Main(string[] args)
         {
             var lines = File.ReadAllLines(args[0]);
-            var startMonth = DateTime.Parse(lines[0]);
-            startMonth = startMonth.AddDays(-startMonth.Day + 1);
-            var diffPerMonth = decimal.Parse(lines[1]);
+            var startWeek = DateTime.Parse(lines[0]).GetWeekStart();
+            const int periodsPerMonth = 4;
+            var diffPerWeek = decimal.Parse(lines[1]) / periodsPerMonth;
             var startingBalance = decimal.Parse(lines[2]);
-            var maxMonthPerDeposit = int.Parse(lines[3]);
+            var maxWeeksPerDeposit = int.Parse(lines[3]) * periodsPerMonth;
             if (lines[4] != "") throw new ArgumentException();
             var oldDeposits = lines.Skip(5).Select(s => s.Split(new[] { ' ' }, 3))
                 .Select(ss => new Deposit(decimal.Parse(ss[1])) { EndDate = DateTime.Parse(ss[0]), Comment = ss.Length >= 3 ? ss[2] : ""}).OrderBy(x => x.EndDate)
                 .ToList();
-            var oldDepositsByEndMonth = oldDeposits.GroupBy(x => x.EndDate.Value.AddDays(-x.EndDate.Value.Day + 1).AddMonths(x.EndDate.Value.Day > 15 ? 1 : 0))
-                .ToDictionary(x => x.Key, x => x.ToList());
+            var oldDepositsByEndWeek = oldDeposits.GroupBy(x => x.EndDate.Value.GetWeekStart()).ToDictionary(x => x.Key, x => x.ToList());
 
-            var periods = Enumerable.Range(0, 48).Select(x => new Period(startMonth.AddMonths(x))).ToList();
+            DateTime counterDt = startWeek;
+
+            var periods = new List<Period>();
+            for (int i = 0; i < 48 * periodsPerMonth; i++)
+            {
+                periods.Add(new Period(counterDt));
+                var w = counterDt.Day / 7 + 1;
+                counterDt = counterDt.AddDays(-counterDt.Day + 1);
+                counterDt = w >= 4 ? counterDt.AddMonths(1) : counterDt.AddDays(7 * w);
+            }
+
 
             foreach (var p in periods)
             {
-                if (oldDepositsByEndMonth.TryGetValue(p.Month, out var d)) 
+                if (oldDepositsByEndWeek.TryGetValue(p.Start, out var d)) 
                     p.Balance += d.Sum(x => x.Amount);
             }
 
@@ -59,63 +68,85 @@ namespace DepositTermCalc
 
             int newDepositsCounter = 0;
             decimal newDepositsBalance = 0;
-            var notReturnedDeposits = new Queue<Deposit>();
+            var notReturnedDeposits = new List<Deposit>();
             var returnedNewDeposits = new List<Deposit>();
             for (int i = 0; i < periods.Count; i++)
             {
-                var p = periods[i];
-                if (i >= maxMonthPerDeposit)
+                var period = periods[i];
+                if (i >= maxWeeksPerDeposit)
                 {
-                    var minPeriod = periods[i - maxMonthPerDeposit];
-                    while (notReturnedDeposits.Count > 0 && notReturnedDeposits.Peek().StartDate <= minPeriod.Month)
+                    var minPeriod = periods[i - maxWeeksPerDeposit];
+                    while (notReturnedDeposits.Count > 0 && notReturnedDeposits.Last().StartDate <= minPeriod.Start)
                     {
-                        var d = notReturnedDeposits.Dequeue();
-                        d.EndDate = p.Month;
+                        var d = notReturnedDeposits.Last();
+                        notReturnedDeposits.RemoveAt(notReturnedDeposits.Count - 1);
+                        d.EndDate = period.Start;
                         newDepositsBalance -= d.Amount;
-                        p.Balance += d.Amount;
+                        period.Balance += d.Amount;
                         returnedNewDeposits.Add(d);
                     }
                 }
 
-                var overBalance = p.Balance + diffPerMonth;
+                var overBalance = period.Balance + diffPerWeek;
                 if (overBalance > 0)
                 {
-                    var deposit = new Deposit(overBalance) { StartDate = p.Month, Comment = "new#"+(++newDepositsCounter) };
-                    notReturnedDeposits.Enqueue(deposit);
+                    var deposit = new Deposit(overBalance) { StartDate = period.Start, Comment = "new#"+(++newDepositsCounter) };
+                    notReturnedDeposits.Insert(0, deposit);
                     newDepositsBalance += overBalance;
-                    p.Balance -= overBalance;
+                    period.Balance -= overBalance;
                 }
                 else if (overBalance < 0)
                 {
                     decimal left = -overBalance;
+
                     while (left > 0 && newDepositsBalance > 0)
                     {
-                        var deposit = notReturnedDeposits.Peek();
+                        var deposit = notReturnedDeposits.Last();
+                        bool tooShortPeriod = (period.Start - deposit.StartDate.Value).TotalDays < 30 - 7 / 2;
+                        if (tooShortPeriod)
+                        {
+                            // can't be a real deposit so better use cash from most recent deposit instead
+                            deposit = notReturnedDeposits.First();
+                        }
+
                         decimal taken = Math.Min(left, deposit.Amount);
                         left -= taken;
-                        p.Balance += taken;
+                        period.Balance += taken;
                         if (taken == deposit.Amount)
                         {
-                            notReturnedDeposits.Dequeue();
+                            notReturnedDeposits.Remove(deposit);
                         }
                         else
                         {
                             deposit.Amount -= taken;
                             var comment = deposit.Comment;
-                            deposit.Comment += "*";
+                            if (tooShortPeriod)
+                                comment += "*";
+                            else
+                                deposit.Comment += "*";
                             deposit = new Deposit(taken) { StartDate = deposit.StartDate, Comment = comment };
                         }
 
-                        deposit.EndDate = p.Month;
-                        returnedNewDeposits.Add(deposit);
+                        deposit.EndDate = period.Start;
+
                         newDepositsBalance -= taken;
+
+                        if (!tooShortPeriod) 
+                            returnedNewDeposits.Add(deposit);
+                        else
+                        {
+                            for (int j = i - 1; j >= 0 && deposit.StartDate < periods[j].Start; j--)
+                            {
+                                periods[j].Balance += deposit.Amount;
+                            }
+                        }
                     }
                 }
             }
 
             foreach (var d in notReturnedDeposits)
             {
-                d.EndDate = periods[periods.Count - 1].Month;
+                d.EndDate = periods[periods.Count - 1].Start;
                 returnedNewDeposits.Add(d);
             }
 
@@ -124,15 +155,15 @@ namespace DepositTermCalc
 
             foreach (var p in periods)
             {
-                Console.WriteLine($"{p.Month:yyyy MM} {p.Balance}");
+                Console.WriteLine($"{p.Start:yyyy-MM-dd} till {p.Start.AddDays(6):MM-dd} {p.Balance}");
                 bool any = false;
-                foreach (var d in returnedNewDeposits.Where(x => x.EndDate == p.Month))
+                foreach (var d in returnedNewDeposits.Where(x => x.EndDate == p.Start))
                 {
-                    Console.WriteLine($"   <<new deposit {d.Amount} from {d.StartDate:yyyy MM} ({GetCelanderMonthDiff(d.EndDate.Value, d.StartDate.Value)}m)");
+                    Console.WriteLine($"   <<new deposit {d.Amount} from {d.StartDate.Value.Date:yyyy-MM-dd} ({GetCelanderMonthDiff(d.EndDate.Value, d.StartDate.Value)}m)");
                     any = true;
                 }
 
-                if (oldDepositsByEndMonth.TryGetValue(p.Month, out var list))
+                if (oldDepositsByEndWeek.TryGetValue(p.Start, out var list))
                 {
                     foreach (var d in list)
                     {
@@ -141,12 +172,16 @@ namespace DepositTermCalc
                     }
                 }
 
-                foreach (var d in returnedNewDeposits.Where(x => x.StartDate == p.Month))
+                foreach (var d in returnedNewDeposits.Where(x => x.StartDate == p.Start))
                 {
-                    Console.WriteLine($"   >>new deposit {GetCelanderMonthDiff(d.EndDate.Value, d.StartDate.Value)}m {d.Amount} `{d.Comment}` till {d.EndDate:yyyy MM}");
+                    Console.WriteLine($"   >>new deposit {GetCelanderMonthDiff(d.EndDate.Value, d.StartDate.Value)}m {d.Amount} `{d.Comment}` till {d.EndDate:yyyy-MM-dd}");
 
                     bool first = true;
-                    foreach (var r in oldDeposits.Concat(returnedNewDeposits).Where(x => x != d && x.EndDate == d.EndDate && x.StartDate <= d.StartDate).OrderBy(x => x.StartDate))
+                    foreach (var r in oldDeposits.Concat(returnedNewDeposits).Where(x => x != d 
+                        && x.EndDate >= d.EndDate.Value.AddDays(-15) 
+                        && x.EndDate <= d.EndDate.Value
+                        && x.StartDate <= d.StartDate)
+                        .OrderBy(x => x.StartDate))
                     {
                         if (first)
                         {
@@ -156,8 +191,8 @@ namespace DepositTermCalc
                         else Console.Write(", ");
 
                         Console.Write(r.StartDate != null
-                            ? $"{GetCelanderMonthDiff(r.EndDate.Value, r.StartDate.Value)}m {r.Amount} `{r.Comment}` {r.StartDate:yyyy MM} till {r.EndDate.Value:yyyy MM}"
-                            : $"{r.Amount} till {r.EndDate:yyyy MM}");
+                            ? $"{GetCelanderMonthDiff(r.EndDate.Value, r.StartDate.Value)}m {r.Amount} `{r.Comment}` {r.StartDate:yyyy-MM-dd} till {r.EndDate.Value:yyyy-MM-dd}"
+                            : $"{r.Amount} till {r.EndDate:yyyy-MM-dd}");
                     }
 
                     if (!first) Console.WriteLine();
@@ -175,6 +210,15 @@ namespace DepositTermCalc
         static int GetCelanderMonthDiff(DateTime date1, DateTime date2)
         {
             return (((date1.Year - date2.Year) * 12) + date1.Month) - date2.Month;
+        }
+
+    }
+
+    static class DateTimeEx
+    {
+        public static DateTime GetWeekStart(this DateTime dt)
+        {
+            return new DateTime(dt.Year, dt.Month, dt.Day / 7 * 7 + 1);
         }
     }
 }
